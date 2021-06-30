9d8e11082ea111842c0bf2fbf8239c3d10ab1413
Lula <lulalentore@gmail.com>
2021-06-29 22:24:36 -0300

    Conexión Firebase

---
+++ src/Firebase/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCXIZ130NpcxI7TUkpuJxnqlyu8D5SqcOQ",
+    authDomain: "storebron-lentore.firebaseapp.com",
+    projectId: "storebron-lentore",
+    storageBucket: "storebron-lentore.appspot.com",
+    messagingSenderId: "480902825124",
+    appId: "1:480902825124:web:a4ed73b7435c66a5fd1231"
+};
+
+const fb = firebase.initializeApp(firebaseConfig);
+
+export const dataBase = fb.firestore();
